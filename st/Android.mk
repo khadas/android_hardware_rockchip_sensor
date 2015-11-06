70f03a29c6ba9e689a0b9d8a564606a6f7f19716
Zhangbin Tong <zebulun.tong@rock-chips.com>
2015-11-06 10:49:43 +0800

    sensor: st: delete unnecessary include path
    Signed-off-by: Zhangbin Tong <zebulun.tong@rock-chips.com>

--- st/Android.mk
+++ st/Android.mk
@@ -46,8 +46,6 @@ LOCAL_SHARED_LIBRARIES := liblog \
 	libcutils \
 	libutils
 
-LOCAL_C_INCLUDES += bionic/libc/kernel/common
-
 #LOCAL_LDFLAGS = $(LOCAL_PATH)/LibFusion_ARM_cpp.a
 LOCAL_LDFLAGS = $(LOCAL_PATH)/MEMSAlgLib_SI_ARM_cpp.a
 
