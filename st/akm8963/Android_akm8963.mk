bbbf41434f2fb50df5999a84a072e35921cff0b7
Hans Yang <yhx@rock-chips.com>
2015-11-12 21:08:17 +0800

    Modify for compatible with 64bit compile
    Change-Id: Iee38c2632be4c1bea7b12b80e3589c010b9359fc
    Signed-off-by: Hans Yang <yhx@rock-chips.com>

--- st/akm8963/Android_akm8963.mk
+++ st/akm8963/Android_akm8963.mk
@@ -1,27 +1,10 @@
-
 LOCAL_PATH:= $(call my-dir)
 
-# libAK8963
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_CLASS := STATIC_LIBRARIES
-LOCAL_MODULE_SUFFIX := .a
-LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_MODULE := libAK8963
-LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 32
 
-include $(BUILD_SYSTEM)/binary.mk
-
-$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/libAK8963/libAK8963.a
-	@mkdir -p $(dir $@)
-	@echo "target StaticLib: $(PRIVATE_MODULE) ($@)"
-	@rm -f $@
-	$(hide) cp $^ $@
-
-# akmd2
-include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := \
-	$(KERNEL_HEADERS) \
 	$(LOCAL_PATH)/libAK8963 
 
 LOCAL_SRC_FILES:= \
@@ -35,10 +18,10 @@ LOCAL_SRC_FILES:= \
 	# Acc_dummy.c
 
 LOCAL_CFLAGS += -DPLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION)
+LOCAL_LDFLAGS += -L$(LOCAL_PATH)/libAK8963 -lAK8963
 LOCAL_MODULE := akmd
 LOCAL_MODULE_TAGS := optional
 LOCAL_FORCE_STATIC_EXECUTABLE := false
-LOCAL_STATIC_LIBRARIES := libAK8963
 LOCAL_SHARED_LIBRARIES := libc libm libz libutils libcutils
 include $(BUILD_EXECUTABLE)
 
--- st/akm8975/Android_akm8975.mk
+++ st/akm8975/Android_akm8975.mk
@@ -1,27 +1,10 @@
-
 LOCAL_PATH:= $(call my-dir)
 
-# libAK8975
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_CLASS := STATIC_LIBRARIES
-LOCAL_MODULE_SUFFIX := .a
-LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_MODULE := libAK8975
-LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 32
 
-include $(BUILD_SYSTEM)/binary.mk
-
-$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/libAK8975/libAK8975.a
-	@mkdir -p $(dir $@)
-	@echo "target StaticLib: $(PRIVATE_MODULE) ($@)"
-	@rm -f $@
-	$(hide) cp $^ $@
-
-# akmd2
-include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := \
-	$(KERNEL_HEADERS) \
 	$(LOCAL_PATH)/libAK8975 
 
 LOCAL_SRC_FILES:= \
@@ -35,10 +18,10 @@ LOCAL_SRC_FILES:= \
 	# Acc_dummy.c
 
 LOCAL_CFLAGS += -DPLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION)
+LOCAL_LDFLAGS += -L$(LOCAL_PATH)/libAK8975 -lAK8975
 LOCAL_MODULE := akmd
 LOCAL_MODULE_TAGS := optional
 LOCAL_FORCE_STATIC_EXECUTABLE := false
-LOCAL_STATIC_LIBRARIES := libAK8975
 LOCAL_SHARED_LIBRARIES := libc libm libz libutils libcutils
 include $(BUILD_EXECUTABLE)
 
