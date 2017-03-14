d7ac8961269151040290408c41a4f02d2482dbcd
Zorro Liu <lyx@rock-chips.com>
2017-03-14 15:42:27 +0800

    mpu: rm mpu, no use
    Change-Id: I46313654c71d18cbfd3f9a0ddd4d0601aae68d69
    Signed-off-by: Zorro Liu <lyx@rock-chips.com>

--- Android.mk
+++ Android.mk
@@ -1,7 +1,5 @@
 # Can't have both mpu and st sensor hal.
-ifeq ($(BOARD_SENSOR_MPU),true)
-include $(call all-named-subdir-makefiles,mpu)
-else ifeq ($(BOARD_SENSOR_MPU_VR),true)
+ifeq ($(BOARD_SENSOR_MPU_VR),true)
 include $(call all-named-subdir-makefiles,mpu_vr)
 else ifeq ($(BOARD_SENSOR_ST),true)
 include $(call all-named-subdir-makefiles,st)
